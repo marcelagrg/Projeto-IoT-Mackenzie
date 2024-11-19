c46fc0a1e7585d1e3a6155fd81cc4a9858793191
marcelagrg <134293767+marcelagrg@users.noreply.github.com>
2024-11-19 08:16:49 -0300

    Update mqtt.js

--- backend-e-frontend/projetoiot/backend/mqtt.js
+++ backend-e-frontend/projetoiot/backend/mqtt.js
@@ -1,16 +1,15 @@
 var mqtt = require('mqtt');
 var options = {
     host: '192.168.0.100',
-    port: 1883, // A porta padrão MQTT (não WebSocket)
-    protocol: 'mqtt', // Usar MQTT em vez de WebSocket
+    port: 1883, // A porta padrão MQTT
+    protocol: 'mqtt', 
 };
 
 var client = mqtt.connect(options);
 
-// Ao conectar ao broker
+// Conectar ao Broker MQTT
 client.on('connect', function () {
     console.log('Conectado ao broker MQTT');
-    // Aqui você pode adicionar tópicos para subscrever ou publicar mensagens
     client.subscribe('esp32/sensordata', function (err) {
         if (!err) {
             console.log('Assinado no tópico!');
@@ -21,7 +20,6 @@ client.on('connect', function () {
     });
 });
 
-// Ao receber uma mensagem
 client.on('message', function (topic, message) {
     // Exibe a mensagem recebida
     console.log('Mensagem recebida no tópico:', topic);
